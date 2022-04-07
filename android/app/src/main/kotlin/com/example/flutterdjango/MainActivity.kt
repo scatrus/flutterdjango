9ffeacc0537d7241782c957d8d8095c4c7108fae
scatrus <12qwasZXwikb6nms>
2022-04-07 19:25:03 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutterdjango/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterdjango
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String buttomName = 'Clicked';
+  int currentIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Academy Manager'),
+        ),
+        body: Center(
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttomName = 'Clicou';
+                    });
+                  },
+                  child: Text(buttomName),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttomName = 'Clicou';
+                    });
+                  },
+                  child: Text(buttomName),
+                ),
+              ],
+            ),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Home',
+              icon: Icon(Icons.home_rounded),
+            ),
+            BottomNavigationBarItem(
+              label: 'Academia',
+              icon: Icon(Icons.sports_kabaddi),
+            ),
+          ],
+          currentIndex: currentIndex,
+          onTap: (int index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
