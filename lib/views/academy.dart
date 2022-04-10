b003707250abfd684da1b94856a0ed5ce63bb915
scatrus <12qwasZXwikb6nms>
2022-04-10 17:40:35 -0300

    update api

--- lib/views/academy.dart
+++ lib/views/academy.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+// import 'home_page.dart';
 
 class Academy extends StatelessWidget {
   const Academy({Key? key}) : super(key: key);
@@ -6,7 +9,17 @@ class Academy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Text('Academia'),
+      ),
+      body: const Text('Academias'),
     );
   }
+}
+
+Future getAcademy() async {
+  var client = http.Client();
+  var response = await client.get(Uri.parse('http://10.0.2.2:8000/academy'));
+
+  debugPrint(response.body);
 }
